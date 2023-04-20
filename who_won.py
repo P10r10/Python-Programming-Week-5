940e7ebfe372ad79fe5d1808b2251362c5ff0cac
P10r10 <asantiago@keemail.me>
2023-04-20 15:31:48 +0100

    who_won

--- who_won.py
+++ who_won.py
@@ -1,1 +1,12 @@
-# TODO
+def count_matching_elements(my_matrix: list, element: int):
+    return sum(row.count(element) for row in my_matrix)
+
+
+def who_won(game_board: list):
+    p1 = count_matching_elements(game_board, 1)
+    p2 = count_matching_elements(game_board, 2)
+    return 0 if p1 == p2 else 1 if p1 > p2 else 2
+
+
+if __name__ == "__main__":
+    print(who_won([[1, 2, 1], [0, 0, 1], [2, 1, 0]]))
