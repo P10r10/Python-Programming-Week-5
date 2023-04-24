d10d3ed35b30422bc8f1ff126f2dd9aafda30bc0
P10r10 <asantiago@keemail.me>
2023-04-24 15:50:00 +0100

    double_items

---
+++ double_items.py
@@ -0,0 +1,9 @@
+def double_items(numbers: list):
+    return list(map(lambda x: 2 * x, numbers))
+
+
+if __name__ == "__main__":
+    numbers = [2, 4, 5, 3, 11, -4]
+    numbers_doubled = double_items(numbers)
+    print("original:", numbers)
+    print("doubled:", numbers_doubled)
