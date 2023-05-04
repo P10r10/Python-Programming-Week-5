0531014cb75a4d72aad45774ca7efaded4d4205a
P10r10 <asantiago@keemail.me>
2023-05-04 15:00:04 +0100

    add_movie

---
+++ add_movie.py
@@ -0,0 +1,16 @@
+# Please write a function named add_movie, which adds a new movie object into
+# a movie database.
+
+def add_movie(database: list, name: str, director: str, year: int,
+              runtime: int):
+    database.append({"name": name,
+                     "director": director,
+                     "year": year,
+                     "runtime": runtime})
+
+
+if __name__ == "__main__":
+    database = []
+    add_movie(database, "Gone with the Python", "Victor Pything", 2017, 116)
+    add_movie(database, "Pythons on a Plane", "Renny Pytholin", 2001, 94)
+    print(database)
